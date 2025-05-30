6ad4ec32010e2d8d3bfcb4b7e5d90528151194eb
mubosher-sk <abdukhalilovmubosher@gmail.com>
2025-05-30 16:07:29 +0900

    main

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
-app.jinja_env.auto_reload = True
-app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.secret_key = 'your-secret-key'
 
 # MongoDB Setup
